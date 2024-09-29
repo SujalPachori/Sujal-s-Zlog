1d3d5387ed8b9b70e03b0c6cf5173be27b6fb76e
SujalPachori <sujalpachori@gmail.com>
2024-09-29 22:53:41 +0530

    changing some stuff and adding gitattributes to git ignore

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,7 +5,7 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-            Skills/Relevant coursework:
+            UtopiaEstate
             </h2>
             <p className='text-gray-500 my-2'>
             {/* • Programming Languages: C/C++, Python, JavaScript, SQL
@@ -14,16 +14,16 @@ export default function CallToAction() {
             • Version Control Systems: Git, GitHub
             • Key courses taken: Data Structures & Algorithm, Python Programming, Probability and Random
                 Processes, Multivariable Calculus, Linear Algebra, Complex analysis, Network Synthesis */}
-                Sujal Pachori creates informative blogs on various topics, including web development, computer science fundamentals, and data structures and algorithms. His content aims to provide insights and guidance for learners and developers, helping them understand essential concepts and improve their skills in these areas. Through his blogs, he shares practical knowledge and problem-solving techniques, offering valuable resources for those looking to enhance their understanding of these subjects.
+                Utopia Real Estate, developed by Sujal Pachori, uses the MERN stack and Tailwind CSS. The app offers advanced authentication with JWT and Google OAuth, allowing users to add property listings with up to six images. It features a robust search function, and Redux Toolkit ensures smooth data management across components.
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://drive.google.com/file/d/17f4EyBefykXlSXmB_9anbETo_Daj8r-6/view" target='_blank' rel='noopener noreferrer'>
-                    Sujal Pachori
+                <a href="https://github.com/SujalPachori/Utopia_Real_Estate" target='_blank' rel='noopener noreferrer'>
+                    Github
                 </a>
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://firebasestorage.googleapis.com/v0/b/zlogs-6b805.appspot.com/o/1726397400232photo%20(1).jpg?alt=media&token=6c7fef84-9c7d-4885-9231-355f76bf282b" />
+            <img src="https://th.bing.com/th/id/R.c855091eaa0d2d5dbbcdb196001c63d4?rik=%2fiI3kxF5YVMUtw&riu=http%3a%2f%2f4.bp.blogspot.com%2f-zxjsKsxjKIk%2fU5vUJPvypfI%2fAAAAAAAAnkA%2fy6Zkwnl6GW8%2fs1600%2fContemporary-Mansions-On-Sunset-Plaza-Drive-LA-passion4luxury-5.jpg&ehk=9PsoEoE4Qy1ZgiP9AMhLvIZ76%2f5MLSf6bZqqVRnEX%2f8%3d&risl=&pid=ImgRaw&r=0" />
         </div>
     </div>
   )
